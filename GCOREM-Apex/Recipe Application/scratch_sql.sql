7fc92d92b23ae8fa856c0838f209aa6647bf5eeb
akubo-benjie <81340043+akubo-benjie@users.noreply.github.com>
2021-03-25 10:37:18 +0800

    Add files via upload

---
+++ GCOREM-Apex/Recipe Application/scratch_sql.sql
@@ -0,0 +1,135 @@
+-- SCRATCH
+
+BEGIN
+
+INSERT INTO RCP_IMPLEMENTS(IMPLEMENTS_NAME,IMPLEMENTS_DESC)
+VALUES (:P23_IMPLEMENTS_NAME, :P23_IMPLEMENTS_DESC);
+
+
+COMMIT;
+
+END;
+
+
+
+
+rename remove to
+- clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+- Exported schema.csv and csv for all of the tables in Nuodb.
+- Downloaded all of the csv and secured for backup.
+- Zip GCOREM project and downloaded for backup.
+-
+-
+
+
+
+
+- Learning/Testing Oracle PL/SQL.
+
+
+
+- Working on Pl/SQL for manually saving the form.
+
+- PL/SQL now saving records.
+
+- Worked on Recipe app.
+- Read Pro Oracle SQL Development and oracle apex documentation(API, Packages).
+- Ongoing: Working on a template for PL/SQL.
+- Ongoing: Working on calculation for total of ingredients and work around for adding multiple steps.
+
+
+
+zip -r project_files.zip project_files
+
+tar -zcvf database_schema.tar.gz dump
+
+
+
+database
+development
+apex
+cloud
+others: oracle technologies
+
+
+
+
+
+back up for linux next week
+march 31. 
+
+
+
+
+
+
+
+
+yield: 10 slics
+serving size: 1 slice
+
+
+
+
+
+- Exported schema csv
+
+
+- Exported schema.csv and csv for all of the tables in Nuodb.
+- Downloaded all of the csv and secured for backup.
+- Zip GCOREM project and downloaded for backup.
+- Fixing exported csv, the output are not organized and looks messed up. (Sort by Create table and References.)
+- Reviewed Modules in GCOREM app(Rails and ApEx)
+- Continued working on DML.
+- Extracting DML from nuodb to be use to populate data in oracle.
+- Converting Functions/procedures to oracle.
+
+
+
+- Read Pro Oracle SQL Development and oracle apex documentation(API, Packages).
+- Learning/Testing Oracle PL/SQL.
+- Worked on Recipe app.
+- Working on PL/SQL template generator.
+- read oracle documentation for collections.
+- Collections enable you to temporarily capture one or more nonscalar values. You can use collections to store rows and columns currently in session state so they can be accessed, manipulated, or processed during a user's specific session. You can think of a collection as a bucket in which you temporarily store and name rows of information.
+- Working around for adding multiple steps.(Collections will be used for the calculation(Total Number of ingredients) for the recipe.)
+
+
+
+
+
+
+
+- Ongoing:
+
+Working on a template for PL/SQL.
+
+Working on calculation for total of ingredients
+
+
+
+- Worked on Recipe app.
+- Learning/Testing Oracle PL/SQL.
+- Read Pro Oracle SQL Development and oracle apex documentation(API, Packages).
+- Worked on collections(Testing, Explore and integrating to recipe app).
+- Created collections for Ingredient list and steps.
+- Testing collections in recipe app.
+
+If ang audio lang want mo i-convert miss pwede mo ni sya ma try para ma play.
+If you just want to convert the audio miss, you can try this online converter.
